8cd6487bd6fe1fcdba0b88fbc8f0829c33282780
AlexanderT <38465150+AlexaTack@users.noreply.github.com>
2021-01-21 19:32:28 +0100

    Add request example

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-const age = 22;
-console.log(age);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 3001;
+app.get('/', (req, res, next) => {
+    res.json({
+        message: 'TS setup!',
+    });
+});
+app.listen(port, () => console.log(`Listening on ${port}.`));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const age: number = 22;
+import express, { Application, Request, Response, NextFunction } from 'express';
 
-console.log(age);
+const app: Application = express();
+
+const port: number = 3001;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.json({
+        message: 'TS setup!',
+    });
+});
+
+
+app.listen(port, () => console.log(`Listening on ${port}.`));
